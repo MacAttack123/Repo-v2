2d1c118a07ee4a611139bd654e621f28f0cb867c
Tracy Moorcroft <tracy.moorcroft@ip-10-11-4-14.eu-west-1.compute.internal>
2019-07-26 15:32:12 +0100

    init repo

---
+++ src/main/java/tracy_groupid_withste/MarvelContactPage.java
@@ -0,0 +1,36 @@
+package tracy_groupid_withste;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class MarvelContactPage {
+
+    static WebDriver driver = new FirefoxDriver();
+    static WebElement element;
+
+
+    public static void main(String[] args) {
+
+
+        System.setProperty("webdriver.gecko.driver", "geckodriver");
+
+        driver.get("https://marvelapp.com/contact-us");
+
+        driver.manage().window().maximize();
+
+        element = driver.findElement(By.name("name"));
+
+        element.click();
+
+        element.sendKeys("Tracy");
+
+        //git driver.close();
+
+
+    }
+
+
+}
